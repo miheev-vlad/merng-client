d5b93eaec5329fddc9ed720b6b684a55ad716730
Mikheev Vladislav <micheev_v_a@mail.ru>
2021-03-12 19:41:21 +0600

    deploy120320211941

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'https://progbook-chat.herokuapp.com/'
+  uri: 'https://murmuring-mesa-28227.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
